2a984d622e939d3790fd0a8dc9619ee3d57f3c37
Keroles Mezare <keroles.mezare2005@gmail.com>
2026-07-26 13:33:57 +0300

    End of section 8

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('Skinet');
+
+
+  // sample test
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
+
+  constructor() {
+    this.http.get(this.baseUrl + 'Products').subscribe({
+      next: (res) => console.log(res),
+    });
+  }
 }
